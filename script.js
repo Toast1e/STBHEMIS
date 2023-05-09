e2507e3a86b6391e4d5f545af24395b2b2283f45
Toast1e <64542429+Toast1e@users.noreply.github.com>
2023-05-09 12:26:04 +0200

    V2
    This version includes only Year 1 of BEng Modules. Javascript still holds capabilities beyond first year. Edit JSON to include more modules, or even more years of modules

--- script.js
+++ script.js
@@ -19,23 +19,23 @@ popupTab.addEventListener("click", function() {
   popupTab.classList.remove("show");
 });
 
-function myFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("subjects-table");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
+// function myFunction() {
+//   var input, filter, table, tr, td, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("subjects-table");
+//   tr = table.getElementsByTagName("tr");
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[0];
+//     if (td) {
+//       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }       
+//   }
+// }
 
 fetch('engData.json')
 .then(response => response.json())
@@ -107,7 +107,8 @@ fetch('engData.json')
     passCell.appendChild(passCheckbox);
     row.appendChild(passCell);
 
-    if (index > 10) {
+    // if (index > 1) {
+      if (1<2) {
       tableBody.appendChild(row);
   } else {
     tableBody.appendChild(row).style.display = 'none';
